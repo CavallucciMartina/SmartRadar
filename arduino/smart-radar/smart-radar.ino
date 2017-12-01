a02da3b530cbba9c8cf3cc0b9513289abb61d2d0
MATTEO MAGNINI <matteo.magnini@studio.unibo.it>
2017-12-01 14:45:19 +0000

    arduino folder and standanrd files created

---
+++ arduino/smart-radar/smart-radar.ino
@@ -0,0 +1,20 @@
+#include "Timer.h"
+
+/*PIN numbers definiton*/
+#define PIN_CONNECTED 2
+#define PIN_SERVO 3
+#define PIN_SERVO 5
+#define PIN_ECHO 8
+#define PIN_TRIG 9
+
+/*Scheduler time period definition*/
+#define TIME_PERIOD 200
+
+/*Tasks instances*/
+Timer timer;
+
+void setup() {
+}
+
+void loop() {
+}
